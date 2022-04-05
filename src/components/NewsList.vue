6c5e13746062ccffb2d5988dbff6e8704c016742
stephya101 <stephy101.sa@gmail.com>
2022-04-04 22:42:17 -0500

    final update

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,11 +1,10 @@
 <template>
 
-<form class="d-flex flex-column justify-content-center">
+<form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
  <div class="input-group mx-sm-3 mb-2">
  <label class="visually-hidden" for="search">Search</label>
  <input type="search" name="search" v-model="searchTerm"
-id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
-search term here" />
+id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
  <button class="btn btn-primary mb-2">Search</button>
  </div>
  <p>You are searching for {{ searchTerm }}</p>
@@ -28,9 +27,27 @@ search term here" />
 export default { 
     data() {
         return {
-            articles: []
+            articles: [],
+            searchTerm: ''
             };
     },
+    methods: {
+        searchNews() {
+        let self = this;
+        fetch('https://newsapi.org/v2/everything?q=' + self.searchTerm + '&language=en', 
+            {headers: {
+                'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            self.articles = data.articles;
+        });
+        }
+        },
 
 created() {
     let self = this;
@@ -50,6 +67,7 @@ created() {
                 });
             }
        };
+       
 </script>
 
 <style>
